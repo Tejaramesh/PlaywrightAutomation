3f656bd289c4d2b74ae72ac098af19e4ce6ec530
Tejaramesh <tejaramesh2000@gmail.com>
2026-08-12 10:36:58 +0530

    updated url and yaml file

--- test.config.ts
+++ test.config.ts
@@ -1,10 +1,10 @@
 export class TestConfig{
-appUrl="http://localhost/opencart/upload/"
+appUrl="https://tutorialsninja.com/demo/"
 //valid login credentials- create your own login account
 email="tejaramesh2000@gmail.com"
 password="Teja@1234"
 //product details
-productName="MacBook"
+productName="HP LP3065"
 productQuantity="2"
 totalPrice="$1,204.00"
 }
